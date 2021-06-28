e814c9bbd592f1eac36d7f160679dddadc53f48d
kimsternator <sskim@ucsd.edu>
2021-06-28 12:41:35 -0700

    Update Initial

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -86,6 +86,7 @@ export default function Skills () {
                         </div>
                     </div>
                 ))}
+
             </div>
             <img
                 src="assets/arrow.png"
