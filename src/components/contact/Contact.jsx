89c5f5c032472a4aa2f4dbd436e22ab1e99c6761
kimsternator <sskim@ucsd.edu>
2021-06-28 12:38:10 -0700

    Initial Commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import "./Contact.scss"
+
+export default function Contact() {
+    return (
+        <div className="contact" id="contact">
+            Hello World
+            <a className="next" href="#intro">
+                <img src="assets/down.png" alt="arrowDown" />
+            </a>
+        </div>
+    )
+}
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,12 @@
+import "./Projects.scss"
+
+export default function Projects() {
+    return (
+        <div className="projects" id="projects">
+            Hello World
+            <a className="next" href="#contact">
+                <img src="assets/down.png" alt="arrowDown" />
+            </a>
+        </div>
+    )
+}
